125ec03601c9c8e1050eadafcd08f43a255e6bf5
chanhduc <chanhduc@497c2c7c-6443-6b61-e55f-58d968213f6b>
2010-07-22 03:07:18 +0000

    Đây là master page, mọi người code trên layout này nhé.

---
+++ trunk/03.Source code/Layout/WebDoanHoi_layout/WebDoanHoi_layout/Masterpage/mtp_three_column.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebDoanHoi_layout.Masterpage
+{
+    public partial class mtp_three_column : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
